459d5dd76b5f2e0880528fd49773c77b4eb662db
Lucas Neves <122572631+lucasnsp@users.noreply.github.com>
2023-11-07 15:02:08 -0300

    Add files via upload

---
+++ List/List/ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApp.swift
+//  List
+//
+//  Created by Lucas Neves dos santos pompeu on 07/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
